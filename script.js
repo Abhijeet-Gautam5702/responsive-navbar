a31bba9b669db3ebcf6fd9ee0e64e9fee8156a83
Abhijeet <abhijeetgautam572@gmail.com>
2022-10-23 14:42:45 +0530

    added responsive navbar
    completed JS logic for opening and closing of the navbar when menu-button is clicked (responsive)

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ let isRotated = false;
 const menuBtn = document.getElementById("menu-icon");
 const ulEl = document.getElementsByTagName("ul");
 const ulStyle = ulEl[0].style;
-// console.log(ulEl[0].style.display);
+const navEl = document.getElementById("navbar");
+const listItem = document.getElementsByClassName("listItem");
+
+navEl.style.overflow = "hidden";
 
 menuBtn.addEventListener("click", function () {
   menuBtn.style.transition = "0.2s";
   if (isRotated === false) {
     menuBtn.style.transform = "rotate(90deg)";
-    ulStyle.display = "flex";
+    navEl.style.removeProperty("overflow");
+    navEl.style.height = "fit-content";
     isRotated = true;
   } else {
     menuBtn.style.transform = "rotate(0deg)";
+    navEl.style.overflow = "hidden";
+    navEl.style.height = "72px";
     isRotated = false;
   }
 });
